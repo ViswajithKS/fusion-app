7a1965c45f3932f762f8a771fd68cdd82fb5b854
Viswajith K S <viswajithks@ug.cusat.ac.in>
2025-01-26 23:01:21 +0530

    added components and assets

---
+++ components/ui/BlankScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+
+export default function BlankScreen(){
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
+            <Text>Blank Screen</Text>
+        </View>
+    );
+}
