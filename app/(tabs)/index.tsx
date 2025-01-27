455f396b1d0e0f511acf1bc161a16116b5b773c6
Viswajith K S <viswajithks@ug.cusat.ac.in>
2025-01-27 21:23:40 +0530

    test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,13 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BlankScreen from "../../components/ui/BlankScreen";
+import SignInPage from "@/components/ui/SignInPage";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      <Stack.Navigator>
-        <Stack.Screen name="Profile" component={BlankScreen} />
-        <Stack.Screen
-          name="Home"
-          component={BlankScreen}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="Profile2" component={BlankScreen} />
-      </Stack.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name="sign in page" component={SignInPage} />
+      <Stack.Screen name="blank screen" component={BlankScreen} />
+    </Stack.Navigator>
   );
 }
--- components/ui/SignInPage.tsx
+++ components/ui/SignInPage.tsx
@@ -1,11 +1,23 @@
 import { useNavigation } from "expo-router";
 import { Button, View } from "react-native";
 
-export default function LoginPage() {
+export default function SignInPage() {
   const navigation = useNavigation();
   return (
-    <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'gray'}}>
-      <Button title="Sign in with google" onPress={() => {navigation.navigate('blank screen')}} />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "gray",
+      }}
+    >
+      <Button
+        title="Sign in with google"
+        onPress={() => {
+          navigation.navigate("blank screen");
+        }}
+      />
     </View>
   );
 }
