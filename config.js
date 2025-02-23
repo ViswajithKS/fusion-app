3aa437fcd8a34c6bb5c519fc9d07b2a189cc0a40
Viswajith K S <viswajithks@ug.cusat.ac.in>
2025-02-23 20:05:04 +0530

    blank

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 import Constants from "expo-constants";
 
+const manifest = Constants.expoConfig ?? Constants.manifest;
+
 const config = {
-  ANDROID_CLIENT_ID: Constants.manifest?.extra?.ANDROID_CLIENT_ID ?? "",
-  IOS_CLIENT_ID: Constants.manifest?.extra?.IOS_CLIENT_ID ?? "",
-  WEB_CLIENT_ID: Constants.manifest?.extra?.WEB_CLIENT_ID ?? "",
-  WEB_CLIENT_SECRET: Constants.manifest?.extra?.WEB_CLIENT_SECRET ?? "",
-  HUGGINGFACE_KEY: Constants.manifest?.extra?.HUGGINGFACE_KEY ?? "",
+  ANDROID_CLIENT_ID: manifest?.extra?.ANDROID_CLIENT_ID ?? "",
+  IOS_CLIENT_ID: manifest?.extra?.IOS_CLIENT_ID ?? "",
+  WEB_CLIENT_ID: manifest?.extra?.WEB_CLIENT_ID ?? "",
+  WEB_CLIENT_SECRET: manifest?.extra?.WEB_CLIENT_SECRET ?? "",
+  HUGGINGFACE_KEY: manifest?.extra?.HUGGINGFACE_KEY ?? "",
 };
 
 export default config;
